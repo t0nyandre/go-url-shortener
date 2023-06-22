4125c93cba40fa014509c05de05e29fc97e14730
t0nyandre <post@tonyandre.co>
2023-06-22 23:35:36 +0200

    impr: added triggers to auto-update the updated_at column

--- migrations/20230607072028_init.down.sql
+++ migrations/20230607072028_init.down.sql
@@ -1,1 +1,3 @@
 drop table urls;
+drop trigger update_urls_updated_at on urls;
+drop function if exists update_updated_at();
--- migrations/20230607072028_init.up.sql
+++ migrations/20230607072028_init.up.sql
@@ -6,3 +6,16 @@ create table urls (
     created_at timestamp not null default now(),
     updated_at timestamp not null default now()
 );
+
+create or replace function update_updated_at()
+returns trigger as $$
+begin
+    new.updated_at = now();
+    return new;
+end;
+$$ language plpgsql;
+
+create trigger update_urls_updated_at
+before update on urls
+for each row
+execute function update_updated_at();
